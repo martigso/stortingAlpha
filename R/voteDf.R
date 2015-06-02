e522bd291b58c8b78f95a8cae98b0bbc27542bd4
Martin G. Søyland <martin_soyland@hotmail.com>
2015-06-02 10:25:51 +0200

    first commit

---
+++ R/voteDf.R
@@ -0,0 +1,30 @@
+#' Return hazard ration from Cox PH model
+#'
+#' A function to transform text into units of voteinfo
+#'
+#'
+#' @param pattern Regex pattern to split the text file on
+#' @param txtvector the text file to make into a data frame
+#'
+#' @return Returns a dataframe with vote info on the specified text file
+#'
+#' @seealso \code{\link{percincrease}}
+#'
+#' @examples
+#' data<-voteDf("Votering[[:punct:]]", alltext)
+#'
+#' @export
+#'
+votedf<-function(pattern, txtvector){
+  tempdf<-data.frame(cbind(vote=as.list(unlist(strapply(txtvector, pattern))),
+                           text=as.list(unlist(strsplit(txtvector, pattern)))[-1]))
+
+  tempdf$enstem<-ifelse(grepl("enstemmig", tempdf$text)==TRUE, 1, 0)
+  tempdf$motstem<-ifelse(grepl("mot [[:digit:]]", tempdf$text)==TRUE | grepl("mod [[:digit:]]", tempdf$text)==TRUE |
+                           grepl("mot[[:digit:]]", tempdf$text)==TRUE | grepl("mod[[:digit:]]", tempdf$text)==TRUE,
+                         1,0)
+  tempdf$rollcall<-ifelse(grepl("nanveopprop", tempdf$text)==TRUE | grepl("navneopraab", tempdf$text)==TRUE, 1,0)
+
+  tempdf
+
+}
